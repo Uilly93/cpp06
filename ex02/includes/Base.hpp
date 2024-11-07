73e8c392f1f30412c37ba00b52ed0a5c7cff8c98
wnocchi <wnocchi@student.42.fr>
2024-11-07 18:00:40 +0100

    ex02 work in preogress

---
+++ ex02/includes/Base.hpp
@@ -0,0 +1,13 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <iostream>
+#include "colors.hpp"
+
+class Base {
+	public:
+		//Destructor
+		~Base();
+};
+
+#endif
---
+++ ex02/src/main.cpp
@@ -0,0 +1,9 @@
+#include "../includes/colors.hpp"
+#include "../includes/Base.hpp"
+#include <cstdlib>
+#include <iostream>
+
+int main(void) {
+	std::srand(time(0));
+	return 0;
+}
